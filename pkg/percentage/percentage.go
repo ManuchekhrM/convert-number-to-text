77f0ba6e5bda7bfd6691951d7c3a778545ccf2ee
Melibaev Manuchekhr <melibaev.m.i@gamil.com>
2023-09-25 16:29:04 +0500

    added int and delete unusable func

--- pkg/percentage/percentage.go
+++ pkg/percentage/percentage.go
@@ -12,7 +12,14 @@ func ConvertNumber(number interface{}, lang string) (string, error) {
 	switch number.(type) {
 	case decimal.Decimal:
 		num := number.(decimal.Decimal)
-		text, err := ConvNumToTextRUWithPercent(num, lang)
+		text, err := ConvNumToTextRuDecimal(num, lang)
+		if err != nil {
+			return "", valueObjects.ErrInvalidLanguage
+		}
+		result = text
+	case int:
+		num := number.(int)
+		text, err := ConvNumToTextRuInt(num, lang)
 		if err != nil {
 			return "", valueObjects.ErrInvalidLanguage
 		}
@@ -22,7 +29,7 @@ func ConvertNumber(number interface{}, lang string) (string, error) {
 	return result, nil
 }
 
-func ConvNumToTextRUWithPercent(num decimal.Decimal, lang string) (string, error) {
+func ConvNumToTextRuDecimal(num decimal.Decimal, lang string) (string, error) {
 	var result string
 
 	needLang, ok := models.Language[lang].(map[int]string)
@@ -53,54 +60,26 @@ func ConvNumToTextRUWithPercent(num decimal.Decimal, lang string) (string, error
 	return result, nil
 }
 
-func ConvNumToTextTJWithPercent(per decimal.Decimal) string {
+func ConvNumToTextRuInt(num int, lang string) (string, error) {
 	var result string
 
-	percent := per.IntPart()
-	decimalPart := per.Sub(decimal.NewFromInt(percent))
+	needLang, ok := models.Language[lang].(map[int]string)
+	if !ok {
+		return "", valueObjects.ErrInvalidLanguage
+	}
+
+	percent := num
 
 	switch {
 	case percent < 10:
-		result = models.OneToTenTJ[percent]
+		result = needLang[percent]
 	case percent > 10 && percent < 20:
-		result = models.TenToNineteenTJ[percent-10]
+		result = needLang[percent-10]
 	default:
 		tensDigit := percent / 10
 		onesDigit := percent % 10
-		result = models.TensTJ[tensDigit] + " " + models.OneToTenTJ[onesDigit]
+		result = needLang[tensDigit] + " " + needLang[onesDigit]
 	}
 
-	if !decimalPart.IsZero() {
-		decimalPartString := int(decimalPart.String()[2] - '0')
-		decimalText := models.OneToTenTJ[decimalPartString]
-		result += " аз даҳ " + decimalText + models.TJ
-	}
-
-	return result
-}
-
-func ConvNumToTextENWithPercent(per decimal.Decimal) string {
-	var result string
-
-	percent := per.IntPart()
-	decimalPart := per.Sub(decimal.NewFromInt(percent))
-
-	switch {
-	case percent < 10:
-		result = models.OneToTenEN[percent]
-	case percent > 10 && percent < 20:
-		result = models.TenToNineteenEN[percent-10]
-	default:
-		tensDigit := percent / 10
-		onesDigit := percent % 10
-		result = models.TensEN[tensDigit] + " " + models.OneToTenEN[onesDigit]
-	}
-
-	if !decimalPart.IsZero() {
-		decimalPartString := int(decimalPart.String()[2] - '0')
-		decimalText := models.OneToTenEN[decimalPartString]
-		result += " point " + decimalText + models.EN
-	}
-
-	return result
+	return result, nil
 }
